926cb73eebfbb30fbbfbf9a64e3177ba94e900e6
Sergey020 <sergeytkachenko020@gmail.com>
2024-02-18 18:12:37 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,20 @@
-const counterValue = 0;
+let counterValue = 0;
+const btnDecr = document.querySelector('button[data-action="decrement"]');
+const btnIncr = document.querySelector('button[data-action="increment"]');
+const valueEl = document.querySelector('#value');
+const addvalue = event => {
+    console.log("+"); 
+    counterValue += 1;
+   
+    console.log(counterValue);
+}
+btnIncr.addEventListener("click",addvalue);
+const minusvalue = event => {
+    console.log("-"); 
+    counterValue -= 1;
+   
+    console.log(counterValue);
+}
+btnDecr.addEventListener("click",minusvalue);
+console.log(counterValue);
+valueEl.textContent = counterValue;
